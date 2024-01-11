6f6d68f4b99d757d0cb1d0f5d629ce40715d9503
Erik Hage <ehage4@gmail.com>
2024-01-10 22:29:07 -0500

    add handlers to place marks

--- src/components/TicTacToe.vue
+++ src/components/TicTacToe.vue
@@ -15,17 +15,21 @@
 
       <v-col class="mb-5" cols="12">
         <v-row justify="center">
-          <v-col cols="3"></v-col>
-          <v-col class="mb-2" cols="6">
-            <v-row v-for="(row, rowIndex) of board">
-              <v-col v-for="(cell, cellIndex) of row" cols="4">
-                <v-card height="100px">
-                  {{ rowIndex }}, {{ cellIndex }}
+          <v-col cols="4"></v-col>
+          <v-col class="mb-2" cols="4">
+            <v-row v-for="(row, rowIndex) of board" :key="rowIndex">
+              <v-col v-for="(cell, cellIndex) of row" cols="4" :key="cellIndex">
+                <v-card
+                    class="d-flex"
+                    height="150px"
+                    @click="place(rowIndex, cellIndex)"
+                    :disabled="cell !== this.BLANK">
+                  {{ cell }}
                 </v-card>
               </v-col>
             </v-row>
           </v-col>
-          <v-col cols="3"></v-col>
+          <v-col cols="4"></v-col>
         </v-row>
       </v-col>
     </v-row>
@@ -34,7 +38,6 @@
 
 <script>
 import logo from '../assets/feralrooster.jpg';
-import {ref, reactive} from 'vue';
 
 const BLANK = '';
 const X = 'X';
@@ -46,19 +49,52 @@ export default {
   name: 'TicTacToe',
 
   data: () => ({
+    X: X,
+    O: O,
+    BLANK: BLANK,
     logo,
     currentPlayer: X,
-    board: reactive([
+    round: 1,
+    board: [
       [BLANK, BLANK, BLANK],
       [BLANK, BLANK, BLANK],
       [BLANK, BLANK, BLANK]
-    ]),
+    ],
   }),
 
   methods: {
     place(x, y) {
       this.board[x][y] = this.currentPlayer;
+      this.checkBoardState();
+      this.switchCurrentPlayer();
     },
+    switchCurrentPlayer() {
+      this.currentPlayer = this.currentPlayer === this.X ? this.O : this.X;
+      this.round = this.round + 1;
+    },
+    checkBoardState() {
+      this.checkWin();
+      this.checkTie();
+    },
+    checkWin() {
+
+    },
+    checkTie() {
+      return this.round >= 10;
+    },
+    reset() {
+      this.round = 1;
+
+    }
   },
 }
 </script>
+<style>
+.cell-x {
+  color: #f00;
+}
+
+.cell-o {
+  color: #00f;
+}
+</style>
