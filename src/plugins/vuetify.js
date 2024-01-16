b0c3c35c0a8e4febccf2c7f057735e3f67b47ca6
Erik Hage <ehage4@gmail.com>
2024-01-16 08:03:15 -0500

    add dark theme, save my eyes

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,11 @@
-// Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+// import colors from 'vuetify/util/colors'
 
-// Vuetify
-import { createVuetify } from 'vuetify'
+import {createVuetify} from 'vuetify'
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+export default createVuetify({
+    theme: {
+        defaultTheme: 'dark'
+    }
+})
