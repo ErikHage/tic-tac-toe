8f25117c1f0dcca8728ebecd484f13ecc8154d99
Erik Hage <ehage4@gmail.com>
2024-01-16 07:32:09 -0500

    extract game history record component

--- src/components/GameHistory.vue
+++ src/components/GameHistory.vue
@@ -3,38 +3,20 @@
     <v-col class="mb-2" cols="12">
       <v-row v-for="(game) in games">
         <v-col cols="12">
-          <v-card :class="getCardClasses(game.winner)">
-            <v-card-text>
-              Game {{game.number}}<br/>
-              <span v-if="game.winner === 'Tie'">Tie Game</span>
-              <span v-else>Winner: {{game.winner}}</span>
-            </v-card-text>
-          </v-card>
+          <game-history-record :game="game"></game-history-record>
         </v-col>
       </v-row>
     </v-col>
   </v-row>
 </template>
 <script>
-import { X, O } from '@/js/constants';
+import GameHistoryRecord from './GameHistoryRecord.vue'
 
 export default {
   name: 'GameHistory',
   props: ['games'],
-  methods: {
-    getCardClasses(winner) {
-      let classes = [];
-      if (winner === X) {
-        classes.push("winner-x");
-      }
-      if (winner === O) {
-        classes.push("winner-o");
-      }
-      if (winner === "Tie") {
-        classes.push("tie");
-      }
-      return classes.join(" ");
-    }
+  components: {
+    GameHistoryRecord,
   },
 }
 </script>
---
+++ src/components/GameHistoryRecord.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card :class="getCardClasses(game.winner)">
+    <v-card-text>
+      Game {{game.number}}<br/>
+      <span v-if="game.winner === 'Tie'">Tie Game</span>
+      <span v-else>Winner: {{game.winner}}</span>
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+import {O, X} from "@/js/constants";
+
+export default {
+  name: 'GameHistoryRecord',
+  props: ['game'],
+  methods: {
+    getCardClasses(winner) {
+      let classes = [];
+      if (winner === X) {
+        classes.push("winner-x");
+      }
+      if (winner === O) {
+        classes.push("winner-o");
+      }
+      if (winner === "Tie") {
+        classes.push("tie");
+      }
+      return classes.join(" ");
+    }
+  }
+}
+</script>
+<style>
+
+</style>
