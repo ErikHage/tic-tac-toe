9374ca9fba601d726c0d97150dfa7450b18ab8af
Erik Hage <ehage4@gmail.com>
2024-01-22 19:43:49 -0500

    display end board state in game history

--- src/components/GameHistory.vue
+++ src/components/GameHistory.vue
@@ -2,20 +2,25 @@
   <v-row>
     <v-col class="mb-2" cols="12">
       <v-row v-for="(game) in games">
-        <v-col cols="12">
-          <game-history-record :game="game"></game-history-record>
+        <v-col cols="9">
+          <game-history-record :game="game" />
+        </v-col>
+        <v-col cols="3">
+          <history-board :board="game.board" />
         </v-col>
       </v-row>
     </v-col>
   </v-row>
 </template>
 <script>
-import GameHistoryRecord from './GameHistoryRecord.vue'
+import GameHistoryRecord from '@/components/GameHistoryRecord.vue'
+import HistoryBoard from "@/components/HistoryBoard.vue";
 
 export default {
   name: 'GameHistory',
   props: ['games'],
   components: {
+    HistoryBoard,
     GameHistoryRecord,
   },
 }
---
+++ src/components/HistoryBoard.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-row
+      v-for="(row, rowIndex) of board"
+      :key="rowIndex"
+      class="board-row">
+    <v-col
+        class="ma-0 mt-1 pa-0 align-center justify-center"
+        v-for="(cell, cellIndex) of row"
+        cols="4"
+        :key="cellIndex">
+      <span :class="getCellTextClasses(cell)">{{ cell }}</span>
+    </v-col>
+  </v-row>
+</template>
+<script>
+import {O, X} from "@/js/constants";
+
+export default {
+  name: "HistoryBoard",
+  props: ['board'],
+  methods: {
+    getCellTextClasses(cell) {
+      let classes = ["history-board-cell"];
+      if (cell === X) {
+        classes.push("cell-x");
+      } else if (cell === O) {
+        classes.push("cell-o");
+      }
+
+      return classes.join(" ");
+    },
+  },
+}
+</script>
+<style>
+.history-board-cell {
+  font-size: 16px;
+}
+
+.cell-x {
+  color: #f00;
+}
+
+.cell-o {
+  color: #00f;
+}
+</style>
